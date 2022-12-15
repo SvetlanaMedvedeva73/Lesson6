56573b817952b30252b72b99ade75b26d855ed56
SvetlanaMedvedeva73 <sweta.medvedev16@yandex.ru>
2022-12-15 09:35:01 +0300

    dz1

---
+++ dz1/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+сколько чисел больше 0 ввёл пользователь. */
+
+int m;
+Console.Write($"Введите количество чисел: ");
+int.TryParse(Console.ReadLine()!, out m);
+
+int[] array = new int[m];
+
+for (int i = 0; i < m; i++)
+{
+    Console.Write($"[{i}] = ");
+    int.TryParse(Console.ReadLine()!, out array[i]);
+}
+int k = 0;
+foreach (var i in array)
+     if (i > 0)
+         k++;
+Console.Write($"{k} чисел больше 0");
+
+
+
